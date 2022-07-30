ae2f529820ff240daa21d8972a73f80961716f7b
Morgan Holcomb <morganholcomb2019@gmail.com>
2022-07-30 15:01:45 -0500

    meaningful

--- __tests__/Potion.test.js
+++ __tests__/Potion.test.js
@@ -1,18 +1,16 @@
-const Potion = require('../lib/Potion');
+const Potion = require('../../../../Downloads/10.3/lib/Potion.js');
 
 test('creates a health potion object', () => {
-    const potion = new Potion('health');
-  
-    expect(potion.name).toBe('health');
-    expect(potion.value).toEqual(expect.any(Number));
-  });
+  const potion = new Potion('health');
 
-//   any number - more flexibility, to avoid testing the random num gen hundreds of times
+  expect(potion.name).toBe('health');
+  expect(potion.value).toEqual(expect.any(Number));
+});
 
 test('creates a random potion object', () => {
-    const potion = new Potion();
-  
-    expect(potion.name).toEqual(expect.any(String));
-    expect(potion.name.length).toBeGreaterThan(0);
-    expect(potion.value).toEqual(expect.any(Number));
-  });
+  const potion = new Potion();
+
+  expect(potion.name).toEqual(expect.any(String));
+  expect(potion.name.length).toBeGreaterThan(0);
+  expect(potion.value).toEqual(expect.any(Number));
+});
